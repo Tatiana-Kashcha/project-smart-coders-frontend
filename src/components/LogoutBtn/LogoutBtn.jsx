984402bb299881fe8dc46380950b0dc2f387a75d
Tatiana Kashcha <tkashcha@gmail.com>
2023-09-28 16:50:37 +0300

    fix name LogoutBtn

---
+++ src/components/LogoutBtn/LogoutBtn.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { ReactComponent as IconLogout } from '../../icons/icons.svg';
+// import { useDispatch } from 'react-redux';
+import * as s from './LogoutBtn.styled';
+
+export const LogoutBtn = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  // const dispatch = useDispatch();
+
+  const onClick = async (values, actions) => {
+    setIsLoading(true);
+    // await dispatch(); // тут буде редакс логаут
+    setIsLoading(false);
+  };
+
+  return (
+    <>
+      <s.LogButton
+        type="button"
+        onClick={onClick}
+        disabled={isLoading}
+        aria-label="Log out"
+      >
+        Log out
+        <s.LogButtonIcon>
+          <IconLogout />
+        </s.LogButtonIcon>
+      </s.LogButton>
+    </>
+  );
+};
