185038d308856972fef03b1faefcda20fd729a59
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-09-28 11:51:22 +0200

    hot fix

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,6 +2,8 @@ import { AuthSection } from 'components/AuthSection/AuthSection';
 import { Description } from 'components/Description/Description';
 import { ReviewsSlider } from 'components/ReviewsSlider/ReviewsSlider';
 
+// MainPage
+
 const MainPage = () => {
   return (
     <>
