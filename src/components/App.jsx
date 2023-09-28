bb257d49c8e338e06b673ce5b45f219f800671a5
Valerii Viun <2455521@gmail.com>
2023-09-28 18:05:27 +0300

    назва комміту

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
 import { lazy } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-const MainPage = lazy(() => import('pages/MainPage'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'));
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/login" element={<h1>Log in</h1>} />
-      <Route path="/register" element={<h1>Sign up</h1>} />
+      {/*  роути: старт main, регистер, логын, паспорт */}
+      <Route path="/" element={<MainPage />}>
+        <Route path="/login" element={<h1>Log in</h1>} />
+        <Route path="/register" element={<h1>Sign up</h1>} />
+      </Route>
 
-      <Route path="*" element={<Navigate to="/" />} />
+      {/*  приват роути (аккаунт, текущий календар, день, месяц, год ) */}
+      <Route path="*" element={<Navigate to="/" />}></Route>
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
---
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,30 @@
+import {
+  Text,
+  NumberWrap,
+  Number,
+  Wrapper,
+  StyledLink,
+  ContentWrap,
+  //   RocketStyled,
+} from './NotFoundPage.styled';
+
+const NotFoundPage = () => {
+  return (
+    <Wrapper>
+      <ContentWrap>
+        <NumberWrap>
+          <Number>4</Number>
+          {/* <RocketStyled /> */}
+          <Number>4</Number>
+        </NumberWrap>
+        <Text>
+          We’re sorry, the page you requested could not be found. Please go back
+          to the homepage.
+        </Text>
+        <StyledLink to="/">Back to home</StyledLink>
+      </ContentWrap>
+    </Wrapper>
+  );
+};
+
+export default NotFoundPage;
