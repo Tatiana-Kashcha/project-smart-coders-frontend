d448d23d39584f6d3cbdceb6c627811774a855c4
Tatiana Kashcha <tkashcha@gmail.com>
2023-09-28 17:18:08 +0300

    Update MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,8 +3,6 @@ import { Description } from 'components/Description/Description';
 import { ReviewsSlider } from 'components/ReviewsSlider/ReviewsSlider';
 
 // MainPage
-// імпорт компонентів для перевірки стилів, поки так:
-import { LogoutBtn } from 'components/LogoutBtn/LogoutBtn';
 
 const MainPage = () => {
   return (
@@ -12,11 +10,6 @@ const MainPage = () => {
       <AuthSection />
       <Description />
       <ReviewsSlider />
-
-      {/* для перевірки стилів */}
-      <div>
-        <LogoutBtn />
-      </div>
     </>
   );
 };
