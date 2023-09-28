376dcb4afcd1163e523b5385aec30e9f7f047c66
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-09-28 11:29:20 +0200

    add fixes

--- src/components/AuthSection/AuthSection.jsx
+++ src/components/AuthSection/AuthSection.jsx
@@ -1,14 +1,20 @@
 import { Link } from 'react-router-dom';
 import * as s from './AuthSection.styled';
+import { Container } from 'components/stylesheet/Container.styled';
 
 export const AuthSection = () => {
   return (
     <s.Section>
-      {/* Logo */}
-      <nav>
-        <Link to="/login">Log in</Link>
-        <Link to="/register">Sign up</Link>
-      </nav>
+      <Container>
+        <s.ContentWrapper>
+          {' '}
+          {/* Logo */}
+          <nav>
+            <Link to="/login">Log in</Link>
+            <Link to="/register">Sign up</Link>
+          </nav>
+        </s.ContentWrapper>
+      </Container>
     </s.Section>
   );
 };
