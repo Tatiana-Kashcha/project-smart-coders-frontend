b778762539f914604c55549ec18dd4b59ed81753
Valerii Viun <2455521@gmail.com>
2023-09-29 17:11:10 +0300

    fixed

--- src/pages/NotFoundPage/NotFoundPage.styled.jsx
+++ src/pages/NotFoundPage/NotFoundPage.styled.jsx
@@ -6,6 +6,7 @@ export const Wrapper = styled.div`
   position: fixed;
   top: 0;
   left: 0;
+  padding: 47px;
   width: 100%;
   height: 100%;
   background-color: ${({ theme }) => theme.colors.background_mode};
@@ -17,15 +18,12 @@ export const ContentWrap = styled.div`
   left: 50%;
   transform: translate(-50%, -50%);
   width: 503px;
-  height: 250px;
 
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.mobile}) {
-    width: 387px;
+    max-width: 252px;
   }
-  /* @media screen and (min-width: ${({ theme }) => theme.breakpoints.table}) {
-    width: 387px;
-  } */
 `;
+
 // NumberWrap
 export const NumberWrap = styled.div`
   display: flex;
@@ -38,9 +36,10 @@ export const NumberWrap = styled.div`
     margin-bottom: -30px;
   }
 `;
+
 // number
 export const Number = styled.p`
-  font-family: ${({ theme }) => theme.fonts.heading};
+  font-style: ${({ theme }) => theme.fonts.heading};
   font-weight: ${({ theme }) => theme.fontWeights.bold};
   color: ${({ theme }) => theme.colors.primary};
 
@@ -49,56 +48,55 @@ export const Number = styled.p`
     line-height: 1.25;
   }
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.mobile}) {
-    font-size: 200px;
-    line-height: 1.5;
+    font-size: 100px;
+    line-height: 1.25;
+  }
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 100px;
+    line-height: 1.25;
   }
 `;
+
 // svg
 export const RocketStyled = styled(Rocket)`
   width: 178px;
   height: 414px;
   margin-left: -30px;
   margin-right: -30px;
-  margin-bottom: -30px;
+  margin-bottom: -15px;
 
   @media screen and (min-width: 768px) {
     max-width: 367px;
     height: 445px;
-
-    /* margin-left: -50px;
-    margin-right: -50px; */
   }
 `;
+
 // text below the number
-export const Text = styled.div`
-  font-family: ${({ theme }) => theme.fonts.text};
+export const Text = styled.p`
+  font-style: ${({ theme }) => theme.fonts.text};
+  /* font-weight: 400; */
   line-height: 1.1;
   text-align: center;
+  font-weight: normal;
 
-  /* font-weight: 500; */
-  font-family: ${({ theme }) => theme.fonts.text};
-  /* line-height: ${({ theme }) => theme.fontSizes.s}; */
-
-  text-align: center;
-  font-weight: ${({ theme }) => theme.fontWeights.normal};
-  font-weight: 400;
-  color: ${({ theme }) => theme.colors.black};
-  /* opacity: 0.7; */
-  margin-bottom: 24px;
-  /* margin-left: auto;
-  margin-right: auto; */
-  margin-top: -80px;
-  width: 281px;
   height: 100%;
+
+  color: ${({ theme }) => theme.colors.black};
+
+  margin-bottom: 24px;
+  margin-top: -100px;
+  margin-left: auto;
+  margin-right: auto;
 
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.table}) {
     width: 387px;
     margin-bottom: 32px;
-    margin-top: -100px;
-    margin-left: -30px;
-    margin-right: -30px;
+  }
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    width: 281px;
   }
 `;
+
 // Link
 export const StyledLink = styled(Link)`
   display: block;
@@ -114,7 +112,6 @@ export const StyledLink = styled(Link)`
   background-color: ${({ theme }) => theme.colors.primary};
   color: ${({ theme }) => theme.colors.white};
   box-shadow: 4px 2px 16px rgba(136, 165, 191, 0.48);
-  /* transition: ${({ theme }) => theme.colors.defaultTransition}; */
 
   &:hover {
     background-color: ${({ theme }) => theme.colors.secondary};
@@ -123,6 +120,11 @@ export const StyledLink = styled(Link)`
 
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.table}) {
     padding: 16px 48px;
+    font-size: 18px;
+    line-height: 1.33;
+  }
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    padding: 14px 32px;
     font-size: 18px;
     line-height: 1.33;
   }
