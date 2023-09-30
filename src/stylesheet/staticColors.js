9c95b397a78c7cf7da884126dc9410cb58849d15
Valerii Viun <2455521@gmail.com>
2023-09-30 17:32:57 +0300

    staticColors

---
+++ src/stylesheet/staticColors.js
@@ -0,0 +1,23 @@
+export const staticColors = {
+  colors: {
+    primary: '#3E85F3',
+    secondary: '#2B78EF',
+    lightBlue: '#E3F3FF',
+    darkBlue: '#CAE8FF',
+    bgBlue: '#DCEBF7',
+    background: '#FFFFFF',
+    white: '#FFFFFF',
+    blackTitle: '#171820',
+    black: '#111111',
+    text: '#343434',
+    mainYellow: '#F3B249',
+    lightYellow: '#FCF0D4',
+    redError: '#DA1414',
+    redErrorLight: '#E74A3B',
+    mainRed: '#EA3D65',
+    lightRed: '#FFD2DD',
+    greenValid: '#3CBC81',
+    muted: '#F7F6F9',
+    closeButton: '#111111',
+  },
+};
