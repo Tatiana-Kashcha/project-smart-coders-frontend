6e555c54a152d67c230f417cee06ab4ca4d938b6
Tatiana Kashcha <tkashcha@gmail.com>
2023-10-01 05:35:03 +0300

    Update MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,8 +2,8 @@ import { AuthSection } from 'components/AuthSection/AuthSection';
 import { Description } from 'components/Description/Description';
 import { ReviewsSlider } from 'components/ReviewsSlider/ReviewsSlider';
 
-import Header from 'components/Header/Header';
-import NotFoundPage from './NotFoundPage/NotFoundPage';
+// import Header from 'components/Header/Header';
+// import NotFoundPage from './NotFoundPage/NotFoundPage';
 
 const MainPage = () => {
   return (
@@ -11,6 +11,7 @@ const MainPage = () => {
       <AuthSection />
       <Description />
       <ReviewsSlider />
+
       {/* <Header />
       <NotFoundPage /> */}
     </>
