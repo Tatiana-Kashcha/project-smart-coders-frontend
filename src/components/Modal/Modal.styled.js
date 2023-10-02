8fbfa2e622cb1b5e5475f881e92d246e36fd8275
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2023-10-02 09:22:12 +0300

    fixed feedbackform style

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -3,15 +3,15 @@ import { ReactComponent as CloseIconBtn } from '../../icons/x-close.svg';
 import { globalTheme } from 'theme';
 
 export const Backdrop = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
   position: fixed;
   top: 0;
   left: 0;
   width: 100vw;
   height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   z-index: 1200;
 
