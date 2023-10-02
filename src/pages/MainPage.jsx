11f4f350f8a8a0ac9850aa86287a4b0e21589b47
Tatiana Kashcha <tkashcha@gmail.com>
2023-10-02 14:57:12 +0300

    upd modal

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,17 +1,13 @@
-// import { AuthSection } from 'components/AuthSection/AuthSection';
-// import { Description } from 'components/Description/Description';
-// import { ReviewsSlider } from 'components/ReviewsSlider/ReviewsSlider';
-
-import  Header from 'components/Header/Header';
+import { AuthSection } from 'components/AuthSection/AuthSection';
+import { Description } from 'components/Description/Description';
+import { ReviewsSlider } from 'components/ReviewsSlider/ReviewsSlider';
 
 const MainPage = () => {
   return (
     <>
-      {/* <AuthSection />
+      <AuthSection />
       <Description />
-      <ReviewsSlider /> */}
-
-      <Header />
+      <ReviewsSlider />
     </>
   );
 };
