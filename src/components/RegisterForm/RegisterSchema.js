68df3a441e6c563e9096f876eec0ad02bd8579e5
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2023-10-03 19:27:03 +0300

    fixed token

--- src/components/RegisterForm/RegisterSchema.js
+++ src/components/RegisterForm/RegisterSchema.js
@@ -1,17 +1,19 @@
 import * as yup from 'yup';
 
 export const RegisterSchema = yup.object().shape({
-    name: yup.string()
-      .required('Required')
-      .min(3, 'Name must contain 3 characters or more')
-      .max(16, 'Name must contain 16 characters or less'),
-    email: yup.string().required('Required').email('Invalid email'),
-    password: yup.string()
-      .required('Required')
-      .min(6, 'Password must contain 6 characters or more')
-      .max(20, 'Password must contain 20 characters or less')
-      .matches(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\\$%\\^&\\*])(?=.{6,60})/,
-        'Password must contain a number, an uppercase and lowercase letter, and a special character'
-      ),
-  });
+  name: yup
+    .string()
+    .required('Required')
+    .min(3, 'Name must contain 3 characters or more')
+    .max(16, 'Name must contain 16 characters or less'),
+  email: yup.string().required('Required').email('Invalid email'),
+  password: yup
+    .string()
+    .required('Required')
+    .min(6, 'Password must contain 6 characters or more')
+    .max(20, 'Password must contain 20 characters or less'),
+  // .matches(
+  //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\\$%\\^&\\*])(?=.{6,60})/,
+  //   'Password must contain a number, an uppercase and lowercase letter, and a special character'
+  // ),
+});
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,3 +1,10 @@
+// import { LoginForm } from "components/LoginForm/LoginForm";
+import { RegisterForm } from 'components/RegisterForm/RegisterForm';
+
 export default function Login() {
-  return <h1>Login page</h1>;
+  return (
+    <RegisterForm />
+    // <LoginForm />
+    // <h1>Login page</h1>;
+  );
 }
