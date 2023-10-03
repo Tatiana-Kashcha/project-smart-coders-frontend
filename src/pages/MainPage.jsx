7cb0242010760a11812ab137b29f00e0df77dccf
Tatiana Kashcha <tkashcha@gmail.com>
2023-10-03 11:59:55 +0300

    Update MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,15 +1,13 @@
-// import { AuthSection } from 'components/AuthSection/AuthSection';
-// import { Description } from 'components/Description/Description';
-// import { ReviewsSlider } from 'components/ReviewsSlider/ReviewsSlider';
-import LoginPage from './LoginPage/LoginPage';
+import { AuthSection } from 'components/AuthSection/AuthSection';
+import { Description } from 'components/Description/Description';
+import { ReviewsSlider } from 'components/ReviewsSlider/ReviewsSlider';
 
 const MainPage = () => {
   return (
     <>
-      <LoginPage />
-      {/* <AuthSection />
+      <AuthSection />
       <Description />
-      <ReviewsSlider /> */}
+      <ReviewsSlider />
     </>
   );
 };
