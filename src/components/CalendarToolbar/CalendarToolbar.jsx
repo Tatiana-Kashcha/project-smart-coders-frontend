f8763286fe318750703067e227e3c0435f464f63
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-10-05 01:35:03 +0200

    add fixes

--- src/components/CalendarToolbar/CalendarToolbar.jsx
+++ src/components/CalendarToolbar/CalendarToolbar.jsx
@@ -2,13 +2,28 @@ import { PeriodPaginator } from 'components/PeriodPaginator/PeriodPaginator';
 import { PeriodTypeSelect } from 'components/PeriodTypeSelect/PeriodTypeSelect';
 
 import * as s from './CalendarToolbar.styled';
+import { useState } from 'react';
 
-export const CalendarToolbar = ({ switchMonthOrDay }) => {
+export const CalendarToolbar = ({ switchMonthOrDay, getPeriodType }) => {
+  const [date, setDate] = useState(null);
+
+  const changeDate = () => {};
+
   return (
     <s.TestDiv>
       <h1>CalendarToolbar в середні якого:</h1>
-      <PeriodPaginator />
+      <PeriodPaginator date={null} periodType={null} changeDate={changeDate} />
       <PeriodTypeSelect switchMonthOrDay={switchMonthOrDay} />
     </s.TestDiv>
   );
 };
+
+/* 1. Компонент рендерить:
+ - PeriodPaginator - дозволяє юзеру змінити дату періоду, задачі за який він хоче подивитись. ✅
+ - PeriodTypeSelect - дозволяє юзеру змінити тип періоду, задачі за який він хоче подивитись. ✅
+2. Компонент отримує тип періоду, та має локальний стейт з датою.
+При зміні дати або типу періоду відбуваеться запит на отримання задач за обраний період, якщо задач з даного періоду досі немає в глобальному стейті.
+Успіх - дані пишуться в глобальний стейт
+Помилка - виводиться відповідне пуш повідомлення.
+ 
+ */
--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,17 +1,15 @@
 import { useNavigate } from 'react-router-dom';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { CalendarToolbar } from 'components/CalendarToolbar/CalendarToolbar';
 import { ChoosedMonth } from 'components/ChoosedMonth/ChoosedMonth';
 import { ChoosedDay } from 'components/ChoosedDay/ChoosedDay';
 
 export default function CalendarPage() {
-  const isFirstVisit = useMemo(() => {
-    const data = sessionStorage.getItem('isFirstVisit');
-    return JSON.parse(data);
-  }, []);
+  const isFirstVisit = JSON.parse(sessionStorage.getItem('isFirstVisit'));
 
-  const [monthOrDay, setMonthOrDay] = useState(!isFirstVisit);
+  // // true = month, false = day
+  const [monthOrDay, setMonthOrDay] = useState(!isFirstVisit); // !isFirstVisit = true
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -45,3 +43,15 @@ export default function CalendarPage() {
     </>
   );
 }
+
+/* "1. Компонент рендериться на маршрут /calendar. ✅
+2. При першому вході на сторінку компонент виконує переадресацію на розширений маршрут /calendar/month/:currentDate для відображення календяря місяця ✅
+2. Сторінка повинна відображатись відповідно до макету на 3х розширеннях(375, 768, 1440) ✅
+3. На сторінці знаходиться модуль CalendarToolbar - з яким користувач може обрати тип періоду, та його інтервал ✅
+4. На сторінці відображаеться один з модулів періоду дат календаря:
+ - ChoosedMonth - дозволяє подивитись всі задачі на місяць, перейти на сторінку одного дня ChoosedDay. ✅
+ - ChoosedDay - дозволяє створювати задачі та розділити ці задачі  на групи по ступеню їх виконання. ✅
+5. При новому відвідуванні додатку та першому вході на сторінку відображаеться модуль ChoosedMonth з розкладкою комірок з датами поточного місяця. ✅
+6. На сторінці повинен здійснюватись запит за завданнями, якщо вони відсутні в глобальному стейті ➖
+7. Успіх - дані записуються у відповідний стейт ➖
+8. Помилка - користувачу показується відповідне пуш-повідомлення" ➖ */
