971fc19c18c1d7a1990a83aeb03e313471f678f0
Valerii Viun <2455521@gmail.com>
2023-10-04 10:33:01 +0300

    Update AccountPage.jsx

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,15 +1,9 @@
 import UserForm from '../components/UserForm/UserForm';
 
-const AccountPage = () => {
-  return <UserForm />;
-};
-
-export default AccountPage;
-
 export default function AccountPage() {
   return (
     <>
-      <h1>AccountPage</h1>
+      <UserForm />
     </>
   );
 }
