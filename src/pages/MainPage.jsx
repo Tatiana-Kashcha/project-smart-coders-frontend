4c9c6455076e7a5109e5a8bbbf99fcf7ea379499
Tatiana Kashcha <tkashcha@gmail.com>
2023-10-04 19:32:36 +0300

    Update MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,15 +1,13 @@
 import { AuthSection } from 'components/AuthSection/AuthSection';
 import { Description } from 'components/Description/Description';
 import { ReviewsSlider } from 'components/ReviewsSlider/ReviewsSlider';
-import UserForm from 'components/UserForm/UserForm';
 
 export default function MainPage() {
   return (
     <>
-      {/* <AuthSection />
+      <AuthSection />
       <Description />
-      <ReviewsSlider /> */}
-      <UserForm />
+      <ReviewsSlider />
     </>
   );
 }
