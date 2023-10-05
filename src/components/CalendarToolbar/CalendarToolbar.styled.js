2d98877a399c5f6fde6b8326becfab6f6ef0e51a
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-10-05 03:45:32 +0200

    delete test width on blocks

--- src/components/CalendarToolbar/CalendarToolbar.styled.js
+++ src/components/CalendarToolbar/CalendarToolbar.styled.js
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
 
 export const TestDiv = styled.div`
-  width: 400px;
-  height: 100px;
   background-color: tomato;
 `;
--- src/components/ChoosedDay/ChoosedDay.styled.js
+++ src/components/ChoosedDay/ChoosedDay.styled.js
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
 
 export const TestDiv = styled.div`
-  width: 400px;
-  height: 400px;
   background-color: orchid;
 `;
