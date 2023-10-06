92f28794e44f8ef26ad4de08f30124d38c16ba66
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2023-10-06 23:15:09 +0300

    add selectors

---
+++ src/redux/reviews/selectors.js
@@ -0,0 +1,3 @@
+export const selectReviews = state => state.reviews.items;
+export const selectIsLoadingReviews = state => state.reviews.isLoading;
+export const selectErrorReviews = state => state.reviews.error;
