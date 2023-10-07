cf4eddef725087885f410de03c877fbb2349c591
Tatiana Kashcha <tkashcha@gmail.com>
2023-10-08 01:29:51 +0300

    Update MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,18 +1,18 @@
-// import { AuthSection } from 'components/AuthSection/AuthSection';
-// import { Description } from 'components/Description/Description';
-// import { ReviewsSlider } from 'components/ReviewsSlider/ReviewsSlider';
+import { AuthSection } from 'components/AuthSection/AuthSection';
+import { Description } from 'components/Description/Description';
+import { ReviewsSlider } from 'components/ReviewsSlider/ReviewsSlider';
 
 // import { TaskToolbar } from 'components/TaskToolbar/TaskToolbar';
-import { TasksColumn } from 'components/TasksColumn/TasksColumn';
+// import { TasksColumn } from 'components/TasksColumn/TasksColumn';
 
 export default function MainPage() {
   return (
     <>
-      {/* <AuthSection />
+      <AuthSection />
       <Description />
-      <ReviewsSlider /> */}
+      <ReviewsSlider />
       {/* <TaskToolbar /> */}
-      <TasksColumn />
+      {/* <TasksColumn /> */}
     </>
   );
 }
