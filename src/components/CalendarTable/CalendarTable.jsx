070dddd9bf0ca3bca4dfd072a6189a453b567405
Valerii Viun <2455521@gmail.com>
2023-10-07 08:56:56 +0300

    назва комміту

--- src/components/CalendarTable/CalendarTable.jsx
+++ src/components/CalendarTable/CalendarTable.jsx
@@ -1,7 +1,58 @@
-export const CalendarTable = () => {
+import React from 'react';
+import moment from 'moment';
+import {
+  CalendarGridWrapper,
+  CellWrapper,
+  DayWrapper,
+  RowInCell,
+} from './CalendarTable.styled';
+
+const currentDate = moment();
+const startDay = currentDate.startOf('month').startOf('week');
+const day = startDay.clone().add(1, 'd');
+
+moment.updateLocale('en', { week: { dow: 1 } });
+
+const CalendarTable = ({ startDay }) => {
+  const currentDayOfMonth = currentDate.format('D');
+  const currentMonth = currentDate.format('MMMM');
+
+  const endDay = currentDate.endOf('month').endOf('week');
+  const calendar = [];
+
+  while (!day.isAfter(endDay)) {
+    calendar.push(day.clone());
+    day.add(1, 'day');
+  }
+
+  window.moment = moment;
+
+  const totalDays = 42;
+  const daysArray = [...Array(totalDays)].map(() => day.add(1, 'day').clone());
+
   return (
-    <h1>
-      CalendarTable - таблиця з днями обраного місяця, відповідно до днів тижня
-    </h1>
+    <div>
+      <h1>Current Month: {currentMonth}</h1>
+      <CalendarGridWrapper>
+        {daysArray.map(dayItem => (
+          <CellWrapper
+            key={dayItem.format('DDMMYYYY')}
+            IsWeekend={dayItem.day() === 6 || dayItem.day() === 0}
+          >
+            <DayWrapper>
+              <RowInCell>
+                {dayItem.format('D') === currentDayOfMonth ? (
+                  <strong>{dayItem.format('D')}</strong>
+                ) : (
+                  dayItem.format('D')
+                )}
+              </RowInCell>
+            </DayWrapper>
+          </CellWrapper>
+        ))}
+      </CalendarGridWrapper>
+    </div>
   );
 };
+
+export default CalendarTable;
--- src/components/ChoosedMonth/ChoosedMonth.jsx
+++ src/components/ChoosedMonth/ChoosedMonth.jsx
@@ -1,13 +1,27 @@
-// import { CalendarTable } from 'components/CalendarTable/CalendarTable';
+import CalendarTable from 'components/CalendarTable/CalendarTable';
 import MonthCalendarHead from '../MonthCalendarHead/MonthCalendarHead';
-
-import * as s from './ChoosedMonth.styled';
+// import moment from 'moment';
+// import { useSelector } from 'react-redux';
+// import { selectTasks } from '../../redux/task/selectors';
+// import { useParams } from 'react-router-dom';
 
 export const ChoosedMonth = () => {
+  // const allTasks = useSelector(selectTasks);
+
+  // const { currentDate } = useParams();
+  // const today = moment(currentDate);
+  // const startDay = today.clone().startOf('month').startOf('week');
+  // const totalDays = 42;
+  // const startDay = moment().startOf('month').startOf('week');
+
   return (
-    <s.TestDiv>
+    <div>
       <MonthCalendarHead />
-      {/* <CalendarTable /> */}
-    </s.TestDiv>
+      <CalendarTable
+        // tasks={allTasks}
+        //  today={today}
+        startDay={'startDay'}
+      />
+    </div>
   );
 };
