966b239ffe00e810db742bffdcdb1f5df96c56bf
AnetRo <beatrice.ccq@gmail.com>
2023-10-08 23:52:29 +0300

    Card N

---
+++ src/components/TaskColumnCard/TaskColumnCard.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+// import TaskToolbar from './TaskToolbar';
+// import TaskModal from './TaskModal';
+import * as s from './TaskColumnCard.styled';
+
+export const TaskColumnCard = (props) => {
+  const { description, avatarUrl, priority } = props;
+  // const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // const handleEditClick = () => {
+  //   setIsModalOpen(true);
+  // };
+
+  // const handleCloseModal = () => {
+  //   setIsModalOpen(false);
+  // };
+
+  return (
+    <s.Card>
+      <s.CardDescr>{description}</s.CardDescr>
+      <s.CardEl>
+        <s.CardAvAndPri>
+          <s.CardAvatar src={avatarUrl} alt="User Avatar" />
+          <s.Priority>{priority}</s.Priority>
+        </s.CardAvAndPri>
+        {/* <TaskToolbar /> */}
+      </s.CardEl>
+      {/* {isModalOpen && <TaskModal onCloseModal={handleCloseModal} />} */}
+    </s.Card>
+  );
+};
+
