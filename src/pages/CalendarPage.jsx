39168f07428e304870bf2011cfe450f9d857dcf0
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-10-09 09:38:41 +0200

    fix bug enabled button

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -28,7 +28,7 @@ export default function CalendarPage() {
     if (isFirstVisit) {
       sessionStorage.setItem('isFirstVisit', 'false');
     } else {
-      setPeriodType('month');
+      setPeriodType('day');
     }
   }, [isFirstVisit]);
 
