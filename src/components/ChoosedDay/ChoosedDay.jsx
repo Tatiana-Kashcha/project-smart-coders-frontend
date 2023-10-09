9ab5f6cb44bde1a3f68924066479098e09253ff2
VeronikaVS90 <vsokolovanika@gmail.com>
2023-10-09 03:44:07 +0300

    Choosed Day

--- src/components/ChoosedDay/ChoosedDay.jsx
+++ src/components/ChoosedDay/ChoosedDay.jsx
@@ -1,12 +1,36 @@
 import { DayCalendarHead } from 'components/DayCalendarHead/DayCalendarHead';
 import { TasksColumnsList } from 'components/TasksColumnsList/TasksColumnsList';
+import { ChoosedDayStyled } from './ChoosedDay.styled';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import {
+  selectTasks,
+  selectIsLoading,
+  selectError,
+} from 'redux/tasks/selectors';
+import Loader from 'components/Loader/Loader';
 
-export const ChoosedDay = () => {
+const ChoosedDay = () => {
+  const tasks = useSelector(selectTasks);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  const { currentDay: targetDate } = useParams();
+  const [filteredTasks, setFilteredTasks] = useState([]);
+
+  useEffect(() => {
+    const filteredTasks = tasks.filter(task => task.date === targetDate);
+    setFilteredTasks(filteredTasks);
+  }, [targetDate, tasks]);
+
   return (
-    <>
-      <h1>ChoosedDay містить в собі:</h1>
+    <ChoosedDayStyled>
+      {isLoading && !error && <Loader />}
       <DayCalendarHead />
-      <TasksColumnsList />
-    </>
+      <TasksColumnsList tasks={filteredTasks} />
+    </ChoosedDayStyled>
   );
 };
+
+export default ChoosedDay;
---
+++ src/components/ChoosedDay/ChoosedDay.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ChoosedDayStyled = styled.div`
+  display: block;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+`;
