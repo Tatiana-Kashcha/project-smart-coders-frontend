c20776824a467c5e461a3f80bbac57accf167be4
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-10-11 15:29:09 +0200

    fix props error and worning

--- src/components/MonthCalendarHead/MonthCalendarHead.jsx
+++ src/components/MonthCalendarHead/MonthCalendarHead.jsx
@@ -2,20 +2,25 @@
 //   return <h1>MonthCalendarHead - дні тижня без дат</h1>;
 // };
 
+import { StyleSheetManager } from 'styled-components';
 import { StyledItem, StyledHead } from './MonthCalendarHead.styled';
 
 const MonthCalendarHead = () => {
   const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
   return (
-    <StyledHead>
-      {daysOfWeek.map(day => (
-        <StyledItem item={day.toUpperCase()} key={day}>
-          <span className="full-name">{day.toUpperCase()}</span>
-          <span className="short-name">{day.toUpperCase().slice(0, 1)}</span>
-        </StyledItem>
-      ))}
-    </StyledHead>
+    <StyleSheetManager
+      shouldForwardProp={prop => prop !== 'calendarorstatistic'}
+    >
+      <StyledHead>
+        {daysOfWeek.map(day => (
+          <StyledItem item={day.toUpperCase()} key={day}>
+            <span className="full-name">{day.toUpperCase()}</span>
+            <span className="short-name">{day.toUpperCase().slice(0, 1)}</span>
+          </StyledItem>
+        ))}
+      </StyledHead>
+    </StyleSheetManager>
   );
 };
 
